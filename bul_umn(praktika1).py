a2bd34e62900879930267c73f000411857ed072b
Susha <alexkamaeva@mail.ru>
2026-02-15 16:05:21 +0500

    Создан проект, добавлена функция булевого умножения матриц(практика 1)

---
+++ bul_umn(praktika1).py
@@ -0,0 +1,13 @@
+import numpy as np
+import ast
+
+def bul_umn(M1, M2):
+    A = np.array(M1)
+    B = np.array(M2)
+    C = (A @ B > 0).astype(int)
+    print(C)
+
+M1 = ast.literal_eval(input())
+M2 = ast.literal_eval(input())
+
+bul_umn(M1, M2)
