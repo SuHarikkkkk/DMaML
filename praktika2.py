100943adce7086a24648f9105075f05ef9baa480
Susha <alexkamaeva@mail.ru>
2026-02-27 13:29:40 +0500

    Доработана вторая практика и сделана третья (диаграмма Хассе)

--- praktika2.py
+++ praktika2.py
@@ -16,46 +16,49 @@ def bul_slozh(M1, M2):
     C = (A + B > 0).astype(int)
     return C
 
-G1=nx.DiGraph(directed=True)
-G2=nx.DiGraph(directed=True)
+k=[1, 2, 3]
+
+r1=[(1,1),(2,2), (3,3), (1,2), (2,3)]
+r2=[(3,2),(1,3),(2,1), (2,2)]
 
 
-G1.add_nodes_from([1, 2, 3, 4, 5, 6, 7])
-r1=[(1,1),(1,3),(1,7),(2,4),(4,3),(3,4),(4,7),(5,5),(5,7),(3,1)]
-r2=[(3,2),(7,4),(4,3),(4,4),(4,5),(5,7),(7,1),(7,2),(1,2),(2,7)]
-
-G1.add_edges_from(r1)
-G2.add_edges_from(r2)
-
-#nx.draw(G1, with_labels=True, node_color='lightblue')
-#plt.show()
-
-#nx.draw(G2, with_labels=True, node_color='lightblue')
-#plt.show()
-
-n = 7
-B = np.zeros((n, n))
-C = np.zeros((n, n))
+n = 3
+BB = np.zeros((n, n))
+CC = np.zeros((n, n))
 
 for t in r1:
-    B[t[0] - 1][t[1] - 1] = 1
+    BB[t[0] - 1][t[1] - 1] = 1
 
 for t in r2:
-    C[t[0] - 1][t[1] - 1] = 1
+    CC[t[0] - 1][t[1] - 1] = 1
 
-N3 = bul_umn(B, C)
-graph3 = N3
-graph = csr_array(graph3)
-print(graph3)
+print("Матрица смежности графа G1", BB)
+print("Матрица смежности графа G2", CC)
+N3 = bul_umn(BB, CC)
+print("Матрица смежности графа композиции G1 и G2", N3)
+N4 = bul_slozh(BB, CC)
 
-print('fffffffffffffffffffffff')
+print("Матрица смежности графа объединения G1 и G2", N4)
+N5 = BB*CC
+print("Матрица смежности графа пересечения G1 и G2", N5)
 
-N4 = bul_slozh(B, C)
-graph4 = N4
-graph4 = csr_array(graph4)
-print(graph4)
 
-N5 = B*C
-graph5 = N5
-graph5 = csr_array(graph5)
-print(graph5)
+G1 = nx.DiGraph(np.matrix(BB))
+G2 = nx.DiGraph(np.matrix(CC))
+G3 = nx.DiGraph(np.matrix(N3))
+G4 = nx.DiGraph(np.matrix(N4))
+G5 = nx.DiGraph(np.matrix(N5))
+
+pos1 = nx.spring_layout(G1, scale=100000,center=[0, 0])
+pos2 = nx.circular_layout(G2, scale=80000,center=[30, 40])
+pos3 = nx.spectral_layout(G3, scale=50000,center=[300, 400])
+pos4 = nx.spiral_layout(G4, scale=40000,center=[35, 4])
+pos5 = nx.spring_layout(G5, scale=60000,center=[3000, 477])
+
+
+nx.draw(G1, pos=pos1, with_labels=True, node_size=200, arrows=True, node_color="blue",font_size=10,font_weight="bold")
+nx.draw(G2, pos=pos2,with_labels=True, node_size=200, arrows=True, node_color="lightblue",font_size=10,font_weight="bold")
+nx.draw(G3, with_labels=True, pos=pos3, node_size=200, arrows=True, node_color="red",font_size=10,font_weight="bold")
+#nx.draw(G4, with_labels=True, node_size=200,pos=pos4, arrows=True, node_color="orange",font_size=10,font_weight="bold")
+nx.draw(G5, with_labels=True, node_size=200,pos=pos5, arrows=True,node_color="green",font_size=10,font_weight="bold")
+plt.show()
